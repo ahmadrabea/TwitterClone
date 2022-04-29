e9ddfd4ce2b0f1743d1e8cd1c1d9c87e34d45ff6
Ahmad Rabea <ahmadrabea10@gmail.com>
2022-04-29 12:46:25 +0300

    adding nextAuth i'm done for today i have headache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import { SessionProvider } from 'next-auth/react'
+import { AppProps } from 'next/app'
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
-
-export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import SideBar from '../Components/SideBar'
 import Feed from '../Components/Feed'
+import Login from '../Components/Login'
+import { getProviders, getSession, useSession } from 'next-auth/react'
+import React, { ReactChild, ReactNode } from 'react'
+import { GetServerSideProps } from 'next'
+import { InferGetServerSidePropsType } from 'next'
 
-const Home: NextPage = () => {
+const Home: NextPage = ({
+  providers,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const { data: session } = useSession()
+  if (!session) return <Login providers={providers} />
+
   return (
     <div className="">
       <Head>
@@ -24,4 +34,14 @@ const Home: NextPage = () => {
   )
 }
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const providers = await getProviders()
+  const session = await getSession(context)
+  return {
+    props: {
+      providers,
+      session,
+    },
+  }
+}
 export default Home
