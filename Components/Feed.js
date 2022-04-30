6ed928d8c157b1e3be031c92598f9b9a547ab76b
Ahmad Rabea <ahmadrabea10@gmail.com>
2022-04-30 05:27:40 +0300

    rendering the tweets on the feed

--- Components/Feed.js
+++ Components/Feed.js
@@ -1,8 +1,24 @@
 import { SparklesIcon } from '@heroicons/react/outline'
 import React from 'react'
 import Input from './Input'
+import { onSnapshot, collection, query, orderBy } from '@firebase/firestore'
+import { useEffect, useState } from 'react'
+import { db } from '../firebase'
+import Post from './Post'
 
 const Feed = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+        (snapshot) => {
+          setPosts(snapshot.docs)
+        }
+      ),
+    [db]
+  )
   return (
     <div className="max-w-2xl flex-grow border-l border-r border-gray-700 text-white sm:ml-[73px] xl:ml-[370px]">
       {/* Start Header */}
@@ -15,6 +31,12 @@ const Feed = () => {
       {/* end Header */}
 
       <Input />
+
+      <div className="pb-72">
+        {posts.map((post) => (
+          <Post key={post.id} id={post.id} post={post.data()} />
+        ))}
+      </div>
     </div>
   )
 }
